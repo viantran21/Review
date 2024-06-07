d705c2eef5785dce2f7e40dbebe70b0090e6e894
Yemam11 <1youssefemam@gmail.com>
2024-06-07 18:10:04 -0400

    Initial Commit

---
+++ push_test.R
@@ -0,0 +1,12 @@
+#' This is a test
+#' We will run some little functions
+#' Then save this as push_test.R
+
+
+myvector <- seq(from = 100, to = 0, by = -7)
+
+myavg <- mean(myvector)
+
+mysum <- sum(myvector)
+
+mysd <- sd(myvector)
