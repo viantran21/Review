deb146c4fd21c60b90b853040cab945fbdead5d1
Yemam11 <1youssefemam@gmail.com>
2024-06-13 14:33:01 -0400

    Added logic to check for a numeric input

--- Assignments/Assignment_2.R
+++ Assignments/Assignment_2.R
@@ -5,5 +5,16 @@
 #' 
 
 #Q1: Prompt the user to enter a 3 digit positive number
-input <- readline("please enter a 3 digit positive number: "))
+
+#as numeric will convert the input the a number, if it is not a number it will convert to NA
+input <- as.numeric(readline("please enter a 3 digit positive number: "))
+
+
+#Q2: Check if the user input is numeric. If not, print an error message and quit.
+
+#if the input is not a number (aka NA) then print an error message and quit with q()
+if (is.na(input)){
+  print("This is not a numeric value")
+  q()
+}
 
