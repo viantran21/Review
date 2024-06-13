67caf1e9e891173eff0dd6a5ca74a2416784fa1b
Yemam11 <1youssefemam@gmail.com>
2024-06-13 14:23:14 -0400

    Created directory and took user input

---
+++ Assignments/Assignment_2.R
@@ -0,0 +1,9 @@
+#' Narcissistic Numbers
+#' Author: Youssef Emam
+#' 
+#' 
+#' 
+
+#Q1: Prompt the user to enter a 3 digit positive number
+input <- readline("please enter a 3 digit positive number: "))
+
