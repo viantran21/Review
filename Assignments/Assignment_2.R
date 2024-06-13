01661da0adc0dc4ec779f27321009de446cdad8a
Yemam11 <1youssefemam@gmail.com>
2024-06-13 15:00:32 -0400

    Added functionality to print the appropriate response if the number is narcissistic

--- Assignments/Assignment_2.R
+++ Assignments/Assignment_2.R
@@ -18,8 +18,49 @@ if (is.na(input)){
   quit()
 } else {
   #Q3: Check if the number is narcissistic
-  # using the modulo operator we can extract the last digit of the number
+  
+  #save the original input
+  original_input <- input
+  
+  #make a variable to store the sum
+  sum_digits <- 0
+  
+  # using the modulo operator we can extract the appropriate digit of the number
   lastdigit <- input %% 10
+  
+  #add the cube of the digit to the running total
+  sum_digits <- sum_digits + (lastdigit^3)
+  
+  #divide the input by 10 and force it to an integer to truncate the last digit
+  input <- as.integer(input / 10)
+  
+  #Use the modulo operator again but find the middle digit (we just removed the last one above)
+  middledigit <- input %% 10
+  
+  #add the cube of the digit to the running total
+  sum_digits <- sum_digits + (middledigit^3)
+  
+  #divide the input by 10 and force it to an integer to truncate the middle digit
+  input <- as.integer(input / 10)
+  
+  #Use the modulo operator again but find the first digit (we just removed the last one above)
+  firstdigit <- input %% 10
+  
+  #add the cube of the digit to the running total
+  sum_digits <- sum_digits + (firstdigit^3)
+  
+  #Q4: Display the result with an appropriate message
+  
+  #Determine if the sum of the cube of the digits is equal to the original input
+  if (sum_digits == original_input){
+    
+    #print an appropriate message
+    cat(original_input, "is a narcissistic number!")
+  } else{
+    
+    #print an appropriate message if it is not a narcissistic number
+    cat(original_input, "is not narcissistic number!")
+  }
 }
 
 
