3eaea3d5be393b6ff3468613c76ae1bdfeb8722a
Yemam11 <1youssefemam@gmail.com>
2024-06-17 16:35:10 -0400

    Wrote more descriptive comments

--- Assignments/Assignment_2.R
+++ Assignments/Assignment_2.R
@@ -1,19 +1,15 @@
 #' Narcissistic Numbers
 #' Author: Youssef Emam
-#' 
-#' 
-#' 
+
 
 # Q1: Prompt the user to enter a 3 digit positive number
 input <- readline("please enter a positive 3 digit number: ")
 
-# as numeric will convert the input the a number, if it is not a number it will convert to NA
-# the supressWarnings function will hide the warning message in the console if NAs are introduced
+# as.numeric will convert the input the a number, if it is not a number it will convert to NA
+# the supressWarnings() function will hide the warning message in the console if NAs are introduced
 # I am doing this since I am handling this case myself and printing an appropriate error message
 input <- suppressWarnings(as.numeric(input))
 
-
-# Q2: Check if the user input is numeric. If not, print an error message and quit.
 
 # Q3: Check if the number is narcissistic
 if (!is.na(input)){
@@ -32,7 +28,7 @@ if (!is.na(input)){
   # add the cube of the digit to the running total
   sum_digits <- sum_digits + (lastdigit^3)
   
-  # divide the input by 10 and force it to an integer to truncate the last digit
+  # divide the input by 10 and coerce it into an integer to truncate the last digit
   # This is so that we can remove the last digit and are left with a 2 digit number to redo the earlier operations with
   input <- as.integer(input / 10)
   
@@ -42,7 +38,7 @@ if (!is.na(input)){
   #add the cube of the digit to the running total
   sum_digits <- sum_digits + (middledigit^3)
   
-  #divide the input by 10 and force it to an integer to truncate the middle digit
+  #divide the input by 10 and force it into an integer to truncate the middle digit
   input <- as.integer(input / 10)
   
   #Use the modulo operator again but find the first digit (we just removed the middle one above)
@@ -66,10 +62,15 @@ if (!is.na(input)){
   }
   
 } else {
-  # If the number is not numeric
+  # Q2: Check if the user input is numeric. If not, print an error message and quit.
   
+  # If the number is not numeric:
   # print an error message
   print("This is not a numeric value")
+  
+  # no stop() function was needed here because this is the last bit of code to be executed
+  # nothing will come after this code block and so the program will stop on its own
+  # including the stop function here would be redundant
 }
 
 
