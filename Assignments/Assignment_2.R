f2204e3d0c4bce9e043e57d912d0662bcedafdcb
viantran21 <viantran1610@gmail.com>
2024-06-21 13:04:09 -0400

    Review Assignment_2.R
    comments, praises and suggestions!

--- Assignments/Assignment_2.R
+++ Assignments/Assignment_2.R
@@ -1,6 +1,7 @@
 #' Narcissistic Numbers
 #' Author: Youssef Emam
 
+## Comments have been made with a ## 
 
 # Q1: Prompt the user to enter a 3 digit positive number
 input <- readline("please enter a positive 3 digit number: ")
@@ -8,8 +9,15 @@ input <- readline("please enter a positive 3 digit number: ")
 # as.numeric will convert the input the a number, if it is not a number it will convert to NA
 # the supressWarnings() function will hide the warning message in the console if NAs are introduced
 # I am doing this since I am handling this case myself and printing an appropriate error message
+
+## This is great! I learned a new function (supressWarnings()) and I will definitely start using this function.  
 input <- suppressWarnings(as.numeric(input))
 
+## When you check if the number is narcissistic (lines 23 and onwards), should you also consider if it is a 
+## 3-digit positive number? (ie. (number >= 1000 | number <= 99)) 
+## I can input a (for example) 4-digit number and it would pass the first checking system (if the input is a number - line 23) 
+## It would output whether the 4-digit number is a narcissistic number (but since your following code only tests for the last three digits,
+## it would only cube the last three digits of that 4 digit number)
 
 # Q3: Check if the number is narcissistic
 if (!is.na(input)){
@@ -46,6 +54,26 @@ if (!is.na(input)){
   
   #add the cube of the digit to the running total
   sum_digits <- sum_digits + (firstdigit^3)
+
+## Line 23 to 56 is a cool method to extract each digit of a 3-digit number! 
+## I found this other method online if you're interested in a different perspective!
+
+#DIFFERENT CODE POV: 
+##To sum the cubes of each individual digit in the 3 digits positive number,
+##we need to split the 3 digits positive number into its individual digits (ie. 123 into 1, 2, and 3)
+
+##We can split the elements of a character vector using the strsplit function by first reading the 3
+##digit positive number as characters
+##CODE:     individual_character <- strsplit(as.character(number), "")
+
+##The strsplit function will display the results as a list. To calculate the sum of cubes of each
+##individual digit, we need to create a vector from the list by using the unlist function
+##We finally need to convert the vector of characters to a numeric vector for further use
+##CODE:    individual_digit <- as.numeric(unlist(individual_character))
+
+##Sum the cubes of each individual digit from the 3-digit positive number (ie. 123 -> 1^3 + 2^3 + 3^3)
+##CODE:    sum_of_cubes <- sum(individual_digit^3)
+
   
   # Q4: Display the result with an appropriate message
   # Determine if the sum of the cube of the digits is equal to the original input
@@ -73,4 +101,6 @@ if (!is.na(input)){
   # including the stop function here would be redundant
 }
 
+## Overall great code! I learned a lot of new functions. I really liked your method in tackling 
+## how to split the 3-digit code into its individual components
 
